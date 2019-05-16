01e5f25d3b5465ce0e855446db74782c4af1fdaa
nabeelsyed <nabeelsyed@10.48.57.49>
2019-05-16 17:31:09 -0400

    Update with StudentID

--- Setter & Getter Practice/src/Main.java
+++ Setter & Getter Practice/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 	
 	private String _name;
+	private int studentID;
 	
 	public void setName (String newName) {
 		_name = newName;
@@ -16,5 +17,13 @@ public class Main {
 	
 	public Main(String n) { // Constructor
 		this._name = n;
+	}
+
+	public int getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
 	}
 }
